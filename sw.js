db541477d54c9f1a7506597138277dc5d6545af3
theblackstaginn <thefarm1965@gmail.com>
2025-12-27 11:42:24 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kindling-cache-v3";
+const CACHE_NAME = "kindling-cache-v4";
 const ASSETS = [
   "./",
   "./index.html",
@@ -47,4 +47,5 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
